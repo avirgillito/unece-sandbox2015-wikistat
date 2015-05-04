07cd1d4dcb573adbb2f037626131b91e01b4ef62
Fernando Reis <fernando.reis@pt.lu>
2015-05-04 18:41:14 +0200

    Added code to save list of articles to text file.

--- scripts/r/whs.R
+++ scripts/r/whs.R
@@ -4,6 +4,14 @@ source("./scripts/r/whs_aux.R")
 downloadWHS(overwrite=FALSE)
 whsData <- loadWHS()
 
-# Get list of wikipedia articles associated to WHS
+# Get list of English wikipedia articles associated to WHS
 downloadWhsArticles(overwrite=FALSE)
 whsArticles <- loadWhsArticles()
+
+# Save list of articles in text file
+artFile <- file("data/articles.txt", open="w", encoding="UTF-8")
+lapply(whsArticles, FUN=function(x) writeLines(x, con=artFile))
+close(artFile)
+
+# Get corresponding articles in Italian
+whsArticles.it <- translateArticlesList(whsArticles)
