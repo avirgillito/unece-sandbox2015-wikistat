8772eb2fd8222bce57496b92d74c92eb2dff0dc2
Bogomil Kovachev <bogomil@unece-sandbox.ichec.ie>
2015-05-06 11:06:51 +0100

    relative location of trnaspose.awk

--- scripts/bash/extract.sh
+++ scripts/bash/extract.sh
@@ -3,5 +3,7 @@
 # the below extracts by article id
 #hadoop dfs -cat $1/* | tr -d "[]," | awk '!($2="")' | awk '!($2="")' | awk -f transpose.awk > $2
 
+transpose_script=$(cd "$( dirname "$0" )" && pwd)/transpose.awk
+
 # the below extracts by name
-hadoop dfs -cat $1/* | tr -d "[]," | awk '!($1="")' | awk '!($1="")' | awk -f transpose.awk > $2
+hadoop dfs -cat $1/* | tr -d "[]," | awk '!($1="")' | awk '!($1="")' | awk -f ${transpose_script} > $2
