54310804dea2fb9b2a1552b8468f45665264ff64
unknown <reisffe@VW-VT-7R01-37.net1.cec.eu.int>
2015-05-06 11:59:21 +0200

    Added code to match WHS scrapped from wikipedia with official list.

--- scripts/r/whs.R
+++ scripts/r/whs.R
@@ -8,10 +8,20 @@ whsData <- loadWHS()
 downloadWhsArticles(overwrite=FALSE)
 whsArticles <- loadWhsArticles()
 
-# Save list of articles in text file
+# Save list of articles in text file for extraction of page views time-series
 artFile <- file("data/articles.txt", open="w", encoding="UTF-8")
 lapply(whsArticles, FUN=function(x) writeLines(x, con=artFile))
 close(artFile)
+
+
+
+# Match WHS scrapped from wikipedia with official list from UNESCO
+match <- sapply(names(whsArticles), FUN=function(x) getClosest(x, whsData[,"site"]))
+whsData$wikipedia <- NA
+whsData$wikipedia[match] <- names(whsArticles)
+
+match <- sapply(whsData[,"site"], FUN=function(x) getClosest(x, names(whsArticles)))
+
 
 # Get corresponding articles in Italian
 whsArticles.it <- translateArticlesList(whsArticles)
