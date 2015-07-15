c8e18a8f0b755f94dc46d5ec746e79129f239434
bogosmith <bogomil@unece-login.ib.sandbox.ichec.ie>
2015-07-15 15:31:10 +0000

    Small bugfix to remove multiple occurences of the header column coming from the parallelism.

--- scripts/bash/extract.sh
+++ scripts/bash/extract.sh
@@ -6,4 +6,4 @@
 transpose_script=$(cd "$( dirname "$0" )" && pwd)/transpose.awk
 
 # the below extracts by name
-hadoop dfs -cat $1/* | tr -d "[]," | awk '!($1="")' | awk '!($1="")' | awk -f ${transpose_script} > $2
+hadoop dfs -cat $1/* | tr -d "[]," | awk '!($1="")' | awk '!($1="")' | awk -f ${transpose_script} | awk ' FNR==1 {for (i =1; i <=NF; i++) if ($i == $1) bad[i]=$i }; { for (i=2; i<=NF; i++) if (i in bad) $i=""; print $0 }' | tr -s " "> $2
